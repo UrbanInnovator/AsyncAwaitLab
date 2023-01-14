d4218d4ce534f75f604521bda33a8077c6da54f8
UrbanInnovator <suburbaninventor@gmail.com>
2023-01-14 01:36:36 -0600

    still testing

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const mostFrequentWord = (text) => {
 
 const findPassword = async () => {
   // Your code goes here
+  const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
+  const poem2name = `poems/${mostFrequentWord(poem1)}.txt`;
+  const poem2 = await readFile(`poems/${poem2name}.txt`, `utf-8`);
+  const poem3name = `poems/${mostFrequentWord(poem2)}.txt`;
+  const poem3 = await readFile(`poems/${poem3name}.txt`, `utf-8`);
+  const password = mostFrequentWord(poem3);
+  console.log(password);
 }
 
 findPassword();
